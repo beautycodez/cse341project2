968f179176b3f4a96625947401373ad59e70862b
beautycodez <angellorl_94@hotmail.com>
2024-05-13 18:27:45 -0500

    summary: post request working - issue: fix the lenght

--- controllers/songs.js
+++ controllers/songs.js
@@ -11,63 +11,138 @@ const getAll = async (req, res) => {
 };
 
 const getSingle = async (req, res) => {
-    //#swagger.tags=['songs']
+  //#swagger.tags=['songs']
   const songId = new ObjectId(req.params.id);
-  const result = await mongodb.getDatabase().db().collection("songs").find({_id:songId});
+  const result = await mongodb
+    .getDatabase()
+    .db()
+    .collection("songs")
+    .find({ _id: songId });
   result.toArray().then((songs) => {
     res.setHeader("Content-Type", "application/json");
     res.status(200).json(songs);
   });
 };
 
-const createsong = async (req,res) => {
-    //#swagger.tags=['songs']
-  const song = {
-    email:req.body.email,
-    songname: req.body.songname,
-    name: req.body.name,
-    ipaddress: req.body.ipaddress
-  };
-  const response = await mongodb.getDatabase().db().collection('songs').insertOne(song);
-  if (response.acknowledged) {
-    res.status(204).send();
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while update the song.');
+const createsong = async (req, res) => {
+  //#swagger.tags=['songs']
+
+  const { band, songs } = req.body;
+
+  try {
+    const bandExists = await mongodb
+      .getDatabase()
+      .db()
+      .collection('songs')
+      .findOne({ band });
+
+    if (bandExists) {
+      res.status(400).json({ message: 'La banda ya existe en la base de datos.' });
+      return;
+    }
+
+    // Crear un array para almacenar las canciones de la banda
+    const songsArray = [];
+
+    // Iterar sobre cada objeto de canción en el array de canciones
+    for (const songData of songs) {
+      // Crear un objeto de canción con los datos proporcionados
+      const songObject = {
+        title: songData.title,
+        time: songData.time,
+        album: songData.album,
+        length: songData.length,
+        genre: songData.genre
+      };
+
+      // Agregar la canción al array de canciones
+      songsArray.push(songObject);
+    }
+
+    // Crear un objeto de banda con el nombre de la banda y el array de canciones
+    const newBand = {
+      band,
+      songs: songsArray
+    };
+
+    // Insertar la banda con sus canciones en la base de datos
+    const response = await mongodb.getDatabase().db().collection('songs').insertOne(newBand);
+
+    if (response.acknowledged) {
+      res.status(201).json(newBand);
+    } else {
+      res.status(500).json({ message: 'Ha ocurrido un error al añadir la banda.' });
+    }
+  } catch (error) {
+    console.error('Error al añadir la banda:', error);
+    res.status(500).json({ message: 'Ha ocurrido un error interno.' });
   }
 };
 
+// const createsong = async (req, res) => {
+//   //#swagger.tags=['songs']
+//   const song = {
+//     band: req.body.band,
+//     songs: req.body.songs,
+//   };
+//   const response = await mongodb
+//     .getDatabase()
+//     .db()
+//     .collection("songs")
+//     .insertOne(song);
+//   if (response.acknowledged) {
+//     res.status(204).send();
+//   } else {
+//     res
+//       .status(500)
+//       .json(response.error || "Some error occurred while update the song.");
+//   }
+// };
+
 const updatesong = async (req, res) => {
-    //#swagger.tags=['songs']
+  //#swagger.tags=['songs']
   const songId = new ObjectId(req.params.id);
   const song = {
     songname: req.body.songname,
-    email:req.body.email,
+    email: req.body.email,
     name: req.body.name,
-    ipaddress: req.body.ipaddress
+    ipaddress: req.body.ipaddress,
   };
-  const response = await mongodb.getDatabase().db().collection('songs').replaceOne({_id:songId}, song);
+  const response = await mongodb
+    .getDatabase()
+    .db()
+    .collection("songs")
+    .replaceOne({ _id: songId }, song);
   if (response.modifiedCount > 0) {
     res.status(204).send();
   } else {
-    res.status(500).json(response.error || 'Some error occured while updating the song');
+    res
+      .status(500)
+      .json(response.error || "Some error occured while updating the song");
   }
 };
 
 const deletesong = async (req, res) => {
-    //#swagger.tags=['songs']
+  //#swagger.tags=['songs']
   const songId = new ObjectId(req.params.id);
-  const response = await mongodb.getDatabase().db().collection('songs').deleteOne({_id: songId});
+  const response = await mongodb
+    .getDatabase()
+    .db()
+    .collection("songs")
+    .deleteOne({ _id: songId });
   if (response.deletedCount > 0) {
     res.status(204).send();
   } else {
-    res.status(500).json(response.error || 'Some error occurred while deleting the song.')
+    res
+      .status(500)
+      .json(response.error || "Some error occurred while deleting the song.");
   }
 };
 
 module.exports = {
-    getAll,
-    getSingle,
-    createsong,
-    updatesong,
-    deletesong
-}
+  getAll,
+  getSingle,
+  createsong,
+  updatesong,
+  deletesong,
+};
