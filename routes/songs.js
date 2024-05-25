7b890c59064934fe562ec1f60c14745c56d6fc3e
beautycodez <angellorl_94@hotmail.com>
2024-05-25 08:27:18 -0500

    final project 2-part 2

--- routes/songs.js
+++ routes/songs.js
@@ -4,15 +4,15 @@ const regValidate = require("../utilities/validation");
 
 const router = express.Router();
 const songsController = require("../controllers/songs");
-const { isAuthenticated } = require("../utilities/authenticate");
 const {requiresAuth} = require('express-openid-connect')
 
 router.get("/",requiresAuth(), songsController.getAll);
 
-router.get("/:id", songsController.getSingle);
+router.get("/:id", requiresAuth(), songsController.getSingle);
 
 router.post(
   "/",
+  requiresAuth(),
   regValidate.registationRules(),
   regValidate.checkRegData,
   songsController.createsong
@@ -20,11 +20,12 @@ router.post(
 
 router.put(
   "/:id",
+  requiresAuth(),
   regValidate.registationRules(),
   regValidate.checkRegData,
   songsController.updatesong
 );
 
-router.delete("/:id", isAuthenticated, songsController.deletesong);
+router.delete("/:id", requiresAuth(), songsController.deletesong);
 
 module.exports = router;
